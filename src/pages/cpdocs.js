979ec92eb21ae7f1f83d6ad89ef2159ae46c27cd
Miguel <maxim467@gmail.com>
2022-08-02 18:30:00 +0200

    Multidex (#9)
    * Added multidex Tip
    
    * Changed Header's background color

--- src/pages/cpdocs.js
+++ src/pages/cpdocs.js
@@ -97,9 +97,8 @@ function InfoTabs() {
 function Header() {
 
   return (
-    <div
+    <div class="headerBackground"
       style={{
-        backgroundColor: ' #25365D',
         textAlign: 'center',
         display: 'block',
         height: '250px',
@@ -108,10 +107,10 @@ function Header() {
         padding: '80px',
         width: '100%'
       }}>
-      <a href="/"><img src="/img/handpoint-logo-hvitt.svg" alt="Logo" width="260px" /></a>
+      <a href="/"><img src="https://handpoint.com/wp-content/uploads/2022/05/Logo.svg" alt="Logo" width="260px" /></a>
       <br></br>
       <h2 style={{
-        color: 'white'
+        color: '#25365d'
       }} >Integrate in-person payments with your software</h2>
     </div>
   );
