ea78624393a42b0938a6ea5ad8eeeb74e0eac6e4
ecunado <ecunado@handpoint.com>
2022-09-21 11:18:10 +0200

    WIP

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ module.exports = {
   tagline: 'Global Payments-Powered Growth for SaaS',
   url: 'https://developer.handpoint.com',
   baseUrl: '/',
-  onBrokenLinks: 'warn',
+  onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: '/img/favicon.png',
   organizationName: 'handpoint', // Usually your GitHub org/user name.
@@ -210,7 +210,7 @@ module.exports = {
       '@docusaurus/plugin-content-docs',
       {
         id: 'ios',
-        includeCurrentVersion: false,
+        includeCurrentVersion: false, // dev: Change to true
         path: 'ios',
         routeBasePath: 'ios',
         sidebarPath: require.resolve('./sidebarsIos.js'),
@@ -224,7 +224,7 @@ module.exports = {
       '@docusaurus/plugin-content-docs',
       {
         id: 'javascript',
-        includeCurrentVersion: false,
+        includeCurrentVersion: false, // dev: Change to true
         path: 'javascript',
         routeBasePath: 'javascript',
         sidebarPath: require.resolve('./sidebarsIos.js'),
@@ -237,7 +237,7 @@ module.exports = {
       '@docusaurus/plugin-content-docs',
       {
         id: 'restapi',
-        includeCurrentVersion: false,
+        includeCurrentVersion: false, // dev: Change to true
         path: 'restapi',
         routeBasePath: 'restapi',
         sidebarPath: require.resolve('./sidebarsIos.js'),
@@ -250,7 +250,7 @@ module.exports = {
       '@docusaurus/plugin-content-docs',
       {
         id: 'windows',
-        includeCurrentVersion: false,
+        includeCurrentVersion: false, // dev: Change to true
         path: 'windows',
         routeBasePath: 'windows',
         sidebarPath: require.resolve('./sidebarsIos.js'),
@@ -262,7 +262,7 @@ module.exports = {
       '@docusaurus/plugin-content-docs',
       {
         id: 'express',
-        includeCurrentVersion: false,
+        includeCurrentVersion: false, // dev: Change to true
         path: 'express',
         routeBasePath: 'express',
         sidebarPath: require.resolve('./sidebarsIos.js'),
@@ -274,7 +274,7 @@ module.exports = {
       '@docusaurus/plugin-content-docs',
       {
         id: 'android',
-        includeCurrentVersion: false,
+        includeCurrentVersion: false, // dev: Change to true
         path: './android',
         routeBasePath: 'android',
         sidebarPath: require.resolve('./sidebarsAndroid.js'),
--- setenv.sh
+++ setenv.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 if [[ $1 == 'prod' ]]
 then
-  sed -i 's/handpoint.io/handpoint.com/g' docusaurus.config.js
-  sed -i "s/developer.handpoint.io/developer.handpoint.com/g" docusaurus.config.js
-  sed -i 's/includeCurrentVersion: true/includeCurrentVersion: false/g' docusaurus.config.js
-  sed -i "s/organizationName: 'handpointdev'/organizationName: 'handpoint'/g" docusaurus.config.js
-  sed -i "s/projectName: 'handpointdev.github.io'/projectName: 'handpoint.github.io'/g" docusaurus.config.js
-  sed -i 's/handpoint.io/handpoint.com/g' src/pages/index.js
-  sed -i 's/developer.handpoint.com/developer.handpoint.com/g' static/CNAME
+  sed -i '' 's/handpoint.io/handpoint.com/g' docusaurus.config.js
+  sed -i '' "s/developer.handpoint.io/developer.handpoint.com/g" docusaurus.config.js
+  sed -i '' 's/includeCurrentVersion: true, \/\/ dev/includeCurrentVersion: false, \/\/ dev/g' docusaurus.config.js
+  sed -i '' "s/organizationName: 'handpointdev'/organizationName: 'handpoint'/g" docusaurus.config.js
+  sed -i '' "s/projectName: 'handpointdev.github.io'/projectName: 'handpoint.github.io'/g" docusaurus.config.js
+  sed -i '' 's/handpoint.io/handpoint.com/g' src/pages/index.js
+  sed -i '' 's/developer.handpoint.com/developer.handpoint.com/g' static/CNAME
 else
-  sed -i 's/handpoint.com/handpoint.io/g' docusaurus.config.js
-  sed -i "s/developer.handpoint.com/developer.handpoint.io/g" docusaurus.config.js
-  sed -i 's/includeCurrentVersion: false/includeCurrentVersion: true/g' docusaurus.config.js
-  sed -i "s/organizationName: 'handpoint'/organizationName: 'handpointdev'/g" docusaurus.config.js
-  sed -i "s/projectName: 'handpoint.github.io'/projectName: 'handpointdev.github.io'/g" docusaurus.config.js
-  sed -i 's/handpoint.com/handpoint.io/g' src/pages/index.js
-  sed -i 's/developer.handpoint.com/handpointdev.github.io/g' static/CNAME
+  sed -i '' 's/handpoint.com/handpoint.io/g' docusaurus.config.js
+  sed -i '' "s/developer.handpoint.com/developer.handpoint.io/g" docusaurus.config.js
+  sed -i '' 's/includeCurrentVersion: false, \/\/ dev/includeCurrentVersion: true, \/\/ dev/g' docusaurus.config.js
+  sed -i '' "s/organizationName: 'handpoint'/organizationName: 'handpointdev'/g" docusaurus.config.js
+  sed -i '' "s/projectName: 'handpoint.github.io'/projectName: 'handpointdev.github.io'/g" docusaurus.config.js
+  sed -i '' 's/handpoint.com/handpoint.io/g' src/pages/index.js
+  sed -i '' 's/developer.handpoint.com/developer.handpoint.io/g' static/CNAME
 fi
